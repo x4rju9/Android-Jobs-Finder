3fba22ad03b812c2add913cd5359adf01b89efc4
Arjun Gangwar <163553130+x4rju9@users.noreply.github.com>
2024-08-01 17:29:37 +0530

    Create keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,21 @@
+from threading import Thread
+from flask import Flask
+from config import Port
+import time
+
+start_time = time.time()
+bot = Flast(__name__)
+
+@bot.route('/')
+def home():
+  end_time = time.time()
+  uptime_secconds = end_time - start_time
+  uptime_minutes = uptime_seconds / 60
+  return f'Bot uptime: {uptime_minutes:.2f} minutes'
+
+def run():
+  bot.run(host = '0.0.0.0', port = 7777)
+
+def keep_alive():
+  t = Thread(target=run)
+  t.start()
