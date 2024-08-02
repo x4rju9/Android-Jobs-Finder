960ac3d4f9842efea7f55e86e1d4dbf1465fbb54
Arjun Gangwar <63835760+4rju9@users.noreply.github.com>
2024-08-02 17:19:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ from re import findall
 from keep_alive import keep_alive
 
 # Credentials.
-api_id = int(os.environ.get('API_ID'))
-api_hash = os.environ.get('API_HASH')
-ss = os.environ.get('STRING_SESSION')
+api_id = int(os.environ.get("API_ID"))
+api_hash = os.environ.get("API_HASH")
+ss = os.environ.get("STRING_SESSION")
 
 # Main
+
 
 def getJobRole(job):
     if "android" in job:
@@ -37,8 +38,9 @@ def getJobRole(job):
         return "full_stack"
     else:
         return "null"
-    
+
     return "null"
+
 
 def isApprovedCreditCard(cc):
     result = findall(r"\bapproved\b", cc)
@@ -48,37 +50,51 @@ def isApprovedCreditCard(cc):
         return False
     return False
 
+
 # Filter Jobs Channels
 fuel_jobs = []
-jobs = os.environ.get('JOBS').split(",")
+jobs = os.environ.get("JOBS").split(",")
 for job in jobs:
-	job = job.strip()
-	if '-' in job:
-		fuel_jobs.append(int(job))
-	elif '' == job:
-		pass
-	else:
-		fuel_jobs.append(job)
+    job = job.strip()
+    if "-" in job:
+        fuel_jobs.append(int(job))
+    elif "" == job:
+        pass
+    else:
+        fuel_jobs.append(job)
 
 # Filter CC Channels
 fuel_credit_card = []
-cc = os.environ.get('CC').split(",")
+cc = os.environ.get("CC").split(",")
 for c in cc:
-	c = c.strip()
-	if '-' in c:
-		fuel_credit_card.append(int(c))
-	elif '' == c:
-		pass
-	else:
-		fuel_credit_card.append(c)
+    c = c.strip()
+    if "-" in c:
+        fuel_credit_card.append(int(c))
+    elif "" == c:
+        pass
+    else:
+        fuel_credit_card.append(c)
 
 # Filter Credit Cards From Each Message.
 def filter_pattern(message):
     import re
-    pattern1 = re.compile(r'(\b\d{16}\b)[a-zA-Z\W]*?(\b\d{2}\b)[a-zA-Z\W]*?(\b\d{2,4}\b)[a-zA-Z\W]*?(\b\d{3,4}\b)', re.DOTALL)
-    pattern2 = re.compile(r'(\b\d{16}\b)[a-zA-Z\W]*?(\b\d{3,4}\b)[a-zA-Z\W]*?(\b\d{2}\b)[a-zA-Z\W]*?(\b\d{2,4}\b)', re.DOTALL)
-    pattern3 = re.compile(r'(\b\d{4}\b).(\b\d{4}\b).(\b\d{4}\b).(\b\d{4}\b)[a-zA-Z\W]*?(\b\d{2}\b)[a-zA-Z\W]*?(\b\d{2,4}\b)[a-zA-Z\W]*?(\b\d{3,4}\b)', re.DOTALL)
-    pattern4 = re.compile(r'(\b\d{4}\b).(\b\d{4}\b).(\b\d{4}\b).(\b\d{4}\b)[a-zA-Z\W]*?(\b\d{3,4}\b)[a-zA-Z\W]*?(\b\d{2}\b)[a-zA-Z\W]*?(\b\d{2,4}\b)', re.DOTALL)
+
+    pattern1 = re.compile(
+        r"(\b\d{16}\b)[a-zA-Z\W]*?(\b\d{2}\b)[a-zA-Z\W]*?(\b\d{2,4}\b)[a-zA-Z\W]*?(\b\d{3,4}\b)",
+        re.DOTALL,
+    )
+    pattern2 = re.compile(
+        r"(\b\d{16}\b)[a-zA-Z\W]*?(\b\d{3,4}\b)[a-zA-Z\W]*?(\b\d{2}\b)[a-zA-Z\W]*?(\b\d{2,4}\b)",
+        re.DOTALL,
+    )
+    pattern3 = re.compile(
+        r"(\b\d{4}\b).(\b\d{4}\b).(\b\d{4}\b).(\b\d{4}\b)[a-zA-Z\W]*?(\b\d{2}\b)[a-zA-Z\W]*?(\b\d{2,4}\b)[a-zA-Z\W]*?(\b\d{3,4}\b)",
+        re.DOTALL,
+    )
+    pattern4 = re.compile(
+        r"(\b\d{4}\b).(\b\d{4}\b).(\b\d{4}\b).(\b\d{4}\b)[a-zA-Z\W]*?(\b\d{3,4}\b)[a-zA-Z\W]*?(\b\d{2}\b)[a-zA-Z\W]*?(\b\d{2,4}\b)",
+        re.DOTALL,
+    )
     result = []
     pattern = 1
     result = pattern1.findall(message)
@@ -91,11 +107,12 @@ def filter_pattern(message):
             if result == []:
                 pattern = 4
                 pattern4.findall(message)
-    return result,pattern
+    return result, pattern
+
 
 def filter_cc(cc):
     matches, pattern = filter_pattern(cc)
-    cc, mm, yy, cvv = "","","",""
+    cc, mm, yy, cvv = "", "", "", ""
     for match in matches:
         if pattern == 1:
             cc = match[0]
@@ -108,18 +125,19 @@ def filter_cc(cc):
             yy = match[3]
             cvv = match[1]
         elif pattern == 3:
-            cc = ''.join(match[0:4])
+            cc = "".join(match[0:4])
             mm = match[4]
             yy = match[5]
             cvv = match[6]
         elif pattern == 4:
-            cc = ''.join(match[0:4])
+            cc = "".join(match[0:4])
             mm = match[5]
             yy = match[6]
             cvv = match[4]
         if len(yy) < 4:
-            yy = '20' + yy
+            yy = "20" + yy
     return f"{cc}|{mm}|{yy}|{cvv}"
+
 
 def create_response(message):
     status = ""
@@ -127,7 +145,7 @@ def create_response(message):
     if "ccn" in message.lower():
         status = " CCN"
     elif "incorrect cvc" in message.lower():
-	status = " CCN"
+        status = " CCN"
     elif "invalid postal code" in message.lower():
         status = " INCORRECT POSTAL"
     elif "declined cvv" in message.lower():
@@ -147,28 +165,29 @@ def create_response(message):
 
     return text_1
 
+
 def main():
     with TelegramClient(StringSession(ss), api_id, api_hash) as client:
 
-        @client.on(events.NewMessage(chats = fuel_jobs))
+        @client.on(events.NewMessage(chats=fuel_jobs))
         async def find_jobs(event):
             job = event.raw_text
             result = getJobRole(job.lower())
-    
+
             if "android" in result:
-                await client.send_message('x4rju9', job, link_preview=False)
+                await client.send_message("x4rju9", job, link_preview=False)
             elif "full_stack" in result:
                 await client.send_message(-1002236063557, job, link_preview=False)
 
-        @client.on(events.NewMessage(chats = fuel_credit_card))
-	@client.on(events.MessageEdited(chats = fuel_credit_card))
+        @client.on(events.NewMessage(chats=fuel_credit_card))
+        @client.on(events.MessageEdited(chats=fuel_credit_card))
         async def cc_leecher(event):
             cc = event.raw_text
-	    mes = cc.lower()
-	    if "wait" in mes or "waiting" in mes or "loading" in mes:
-	        return
+            mes = cc.lower()
+            if "wait" in mes or "waiting" in mes or "loading" in mes:
+                return
             result = isApprovedCreditCard(cc.lower())
-    
+
             if result:
                 splited = create_response(cc).split("\n")
                 cc = ""
@@ -179,6 +198,7 @@ def main():
         # start bot
         client.start()
         client.run_until_disconnected()
+
 
 if __name__ == "__main__":
     keep_alive()
