582d6c7efff35adb2f3833ff4eb96bcf8c1e644b
Arjun Gangwar <63835760+4rju9@users.noreply.github.com>
2024-10-06 18:56:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -990,8 +990,19 @@ def main():
         @client.on(events.MessageEdited(chats=fuel_movies))
         async def find_movies(event):
             try:
-                if event.video or event.document:
-                    await client.forward_messages(-1002002129675, event.message)
+                caption = ""
+                if event.text:
+                    caption = event.text
+                if event.message:
+                    if event.message.caption:
+                        caption = event.message.caption
+                video = ""
+                if event.video:
+                    video = event.video
+                if event.document:
+                    video = event.document
+                if video:
+                    await client.send_file(-1002002129675, caption = caption, file = video)
             except:
                 pass
         
